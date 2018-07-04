524e9eda3e0c96c3f7d381e46c76bff429f563da
Luca Olivieri <gliolivieri@live.it>
2018-07-04 18:51:41 +0200

    Updated geolacalization API

--- AccessLogger.php
+++ AccessLogger.php
@@ -6,54 +6,56 @@ error_reporting(E_ALL);
 // Include and instantiate the class.
 require_once 'Mobile_Detect.php';
 
-
-class AccessLogger 
+class AccessLogger
 {
     private $sql;
     private $table;
+    private $apiKey;
     // Make them visible in case other portions of code needs them
     public $IP;
     public $Detect;
     public $Headers;
     public $Geolocation;
     public $UserAgent;
-    
+
     // The table is needed because each page has its own
     // instead of one enormous one with all pages
-    function __construct ($table) {
+    public function __construct($table)
+    {
         // Database connection
         require_once 'login.php';
-        $this->sql = new mysqli($hostName, $userName, $passWord, $dataBase);        
+        $this->sql = new mysqli($hostName, $userName, $passWord, $dataBase);
         if ($this->sql->connect_error) {
             die($this->sql->connect_error);
-        }        
+        }
         $this->table = $table;
+        $this->apiKey = $apiKey;
     }
 
-    function log() {
+    public function log()
+    {
         // Getting information
         $this->Detect = new Mobile_Detect;
         $device = "";
         // First load the page
-        if($this->Detect->isMobile() && !$this->Detect->isTablet()){            
+        if ($this->Detect->isMobile() && !$this->Detect->isTablet()) {
             $device = "mobile";
-        } else {            
+        } else {
             $device = "PC";
         }
         // Just check if tablet
-        if($this->Detect->isTablet()) {
+        if ($this->Detect->isTablet()) {
             $device = "tablet";
         }
-
 
         $this->IP = $_SERVER['REMOTE_ADDR'];
         $this->Headers = apache_request_Headers();
         $this->UserAgent = preg_replace("/\'/", "\'", $this->Headers['User-Agent']);
 
-        $geolocate = json_decode(file_get_contents('http://freegeoIP.net/json/' . $this->IP), true);
-        $this->Geolocation = preg_replace("/\'/", "\'",  $geolocate['country_name'] . ', ' . $geolocate['region_name'] . ', ' . $geolocate['city']);
+        $geolocate = json_decode(file_get_contents("http://api.ipstack.com/$this->IP?access_key=$this->apiKey&output=json&legacy=1"), true);
+        $this->Geolocation = preg_replace("/\'/", "\'", $geolocate['country_name'] . ', ' . $geolocate['region_name'] . ', ' . $geolocate['city']);
 
-        $query = "INSERT INTO `$this->table` VALUES(NULL, NULL, '$this->IP', '$device', '$this->Geolocation', '$this->UserAgent')";        
+        $query = "INSERT INTO $this->table VALUES(NULL, NULL, '$this->IP', '$device', '$this->Geolocation', '$this->UserAgent')";
         if (!$this->sql->query($query)) {
             echo "Could not insert into database: " . $this->sql->error;
         }
@@ -61,4 +63,3 @@ class AccessLogger
         $this->sql->close();
     }
 }
-?>
